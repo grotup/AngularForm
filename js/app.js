5849289fdd28f92fec7fdad6b68ea35365d274e5
Nb <nael.sylvain@gmail.com>
2014-09-29 08:13:38 +0200

    Ajout utilisation $http + rest API

--- js/app.js
+++ js/app.js
@@ -1,90 +1,98 @@
-var todo = angular
-				.module('todo', ['LocalStorageModule'])
-				.controller('todoctrl', [
-					'$scope',
-					'localStorageService', 
-					function ($scope, localStorageService){
-						$scope.title = 'My Todo List';
+var app = angular.module('app', ['LocalStorageModule', 'ngRoute']);
 
-						if(!localStorageService.get('tasks')){
-							$scope.tasks = [];
-						}else{
-							$scope.tasks = localStorageService.get('tasks');
-						}
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/todo', {
+			templateUrl	: 'partials/todo.html',
+			controller  : 'todoCtrl'
+		})
+		.when('/crud', {
+			templateUrl : 'partials/crud.html',
+			controller : 'crud'
+		})
+		.otherwise({
+			redirectTo : '/todo'
+		});
+});
 
-						$scope.users = [
-								{"username" : "Sylvain"}, 
-								{"username": "Anastasia"},
-								{"username" : "Jean Michel"}
-							];
+app.controller('todoctrl', ['$scope','localStorageService', function ($scope, localStorageService){
+	$scope.title = 'My Todo List';
 
-						// pour éviter une valeur vide dans les filtres, on définit une valeur à l'utilisateur 
-						// @see http://stackoverflow.com/questions/12654631/why-does-angularjs-include-an-empty-option-in-select
-						if(!localStorageService.get('user_selected')){
-							$scope.filter = {user : $scope.users[0]};
-						}else{
-							$scope.filter = {user : localStorageService.get('user_selected')};
-						}
-						
-						$scope.add_task = function(){
-							$scope.tasks.push(
-								{
-									'title' : $scope.todo_lb,
-									'done'  : false,
-									"user"  : $scope.filter.user
-								});
-							$scope.todo_lb = '';
-							localStorageService.set('tasks',$scope.tasks);
-						}
-						
-						$scope.archiver = function(data){
-							if(!data){
-								return;
-							}
-							
-							data.done = true;
-							localStorageService.set('tasks',$scope.tasks);
-						}
-						
-						$scope.supprimer_tache = function(index){
-							if(index < 0){
-								return;
-							}
-							$scope.tasks.splice(index,1);
-							localStorageService.set('tasks',$scope.tasks);
-						}
-						
-						$scope.delete_all_tasks = function(){
-							$scope.tasks = [];
-							localStorageService.set('tasks',$scope.tasks);
-						}
+	if(!localStorageService.get('tasks')){
+		$scope.tasks = [];
+	}else{
+		$scope.tasks = localStorageService.get('tasks');
+	}
 
-						$scope.update_user = function(){
-							localStorageService.set('user_selected',$scope.filter.user);	
-						}
-					}
-				])
-				.controller('crud', [
-					'$scope',
-					function($scope){
-						$scope.title = 'Mon test CRUD';
+	$scope.users = [
+			{"username" : "Sylvain"}, 
+			{"username": "Anastasia"},
+			{"username" : "Jean Michel"}
+		];
 
-						$scope.books = [
-							{
-								"title" : "L'alchimiste",
-								"author": "Paulo Coelho",
-								"year"  : "1997"
-							},
-							{
-								"title" : "Bouquin de test",
-								"author": "Jean Michel",
-								"year"  : "2014"
-							}
-						];
+	// pour éviter une valeur vide dans les filtres, on définit une valeur à l'utilisateur 
+	// @see http://stackoverflow.com/questions/12654631/why-does-angularjs-include-an-empty-option-in-select
+	if(!localStorageService.get('user_selected')){
+		$scope.filter = {user : $scope.users[0]};
+	}else{
+		$scope.filter = {user : localStorageService.get('user_selected')};
+	}
+	
+	$scope.add_task = function(){
+		$scope.tasks.push(
+			{
+				'title' : $scope.todo_lb,
+				'done'  : false,
+				"user"  : $scope.filter.user
+			});
+		$scope.todo_lb = '';
+		localStorageService.set('tasks',$scope.tasks);
+	}
+	
+	$scope.archiver = function(data){
+		if(!data){
+			return;
+		}
+		
+		data.done = true;
+		localStorageService.set('tasks',$scope.tasks);
+	}
+	
+	$scope.supprimer_tache = function(index){
+		if(index < 0){
+			return;
+		}
+		$scope.tasks.splice(index,1);
+		localStorageService.set('tasks',$scope.tasks);
+	}
+	
+	$scope.delete_all_tasks = function(){
+		$scope.tasks = [];
+		localStorageService.set('tasks',$scope.tasks);
+	}
 
-						$scope.add_book = function(){
-							alert('Test !');
-							//$scope.books.push(data);
-						};
-					}
-				]);
+	$scope.update_user = function(){
+		localStorageService.set('user_selected',$scope.filter.user);	
+	}
+}]);
+app.controller('crud', ['$scope',function($scope){
+	$scope.title = 'Mon test CRUD';
+
+	$scope.books = [
+		{
+			"title" : "L'alchimiste",
+			"author": "Paulo Coelho",
+			"year"  : "1997"
+		},
+		{
+			"title" : "Bouquin de test",
+			"author": "Jean Michel",
+			"year"  : "2014"
+		}
+	];
+
+	$scope.add_book = function(){
+		alert('Test !');
+		//$scope.books.push(data);
+	};
+}]);
